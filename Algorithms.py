66fffb2d6b0b75e5e2ae077df3bc6ae8f9fa4f96
Michal Mano <t-michalmano@microsoft.com>
2024-02-20 14:45:44 +0200

    initial

--- Algorithms.py
+++ Algorithms.py
@@ -4,6 +4,7 @@ from IPython.display import clear_output
 from DragonBallEnv import DragonBallEnv
 from typing import List, Tuple
 import heapdict
+
 class Node():
     def __init__(self, parent, state_tuple, action, parent_cost, node_cost,env) -> None:
         self.env = env
@@ -53,27 +54,88 @@ class BFSAgent():
             closed.append(node.state_tuple)
             expanded += 1
             print('expanded node:', node.state_tuple, '\n')
-            for action, (state, cost, terminated) in env.succ(node.state_tuple).items():
-                child_node = Node(node, state, action, node.cumulative_cost, cost, self.env)
-                child_node.state_tuple = (child_node.state_tuple[0], node.state_tuple[1], node.state_tuple[2])
-                if child_node.state_tuple[0] == env.d1[0]:
-                    child_node.state_tuple = (child_node.state_tuple[0], True, child_node.state_tuple[2])
-                if child_node.state_tuple[0] == env.d2[0]:
-                    child_node.state_tuple = (child_node.state_tuple[0], child_node.state_tuple[1], True)
-                if child_node.state_tuple not in closed and child_node.state_tuple not in [open_node.state_tuple for open_node in open]:
-                    if env.is_final_state(child_node.state_tuple):
-                        return get_actions(child_node), child_node.cumulative_cost, expanded
-                    if not terminated and child_node.state_tuple != node.state_tuple:
-                        open.append(child_node)
+            row, col = self.env.to_row_col(node.state_tuple)
+            if self.env.desc[row, col] != b"H":
+                for action, (state, cost, terminated) in env.succ(node.state_tuple).items():
+                    child_node = Node(node, state, action, node.cumulative_cost, cost, self.env)
+                    child_node.state_tuple = (child_node.state_tuple[0], node.state_tuple[1], node.state_tuple[2])
+                    if child_node.state_tuple[0] == env.d1[0]:
+                        child_node.state_tuple = (child_node.state_tuple[0], True, child_node.state_tuple[2])
+                    if child_node.state_tuple[0] == env.d2[0]:
+                        child_node.state_tuple = (child_node.state_tuple[0], child_node.state_tuple[1], True)
+                    if child_node.state_tuple not in closed and child_node.state_tuple not in [open_node.state_tuple for open_node in open]:
+                        if env.is_final_state(child_node.state_tuple):
+                            return get_actions(child_node), child_node.cumulative_cost, expanded
+                        if child_node.state_tuple != node.state_tuple:
+                            open.append(child_node)
 
 
 
 class WeightedAStarAgent():
     def __init__(self) -> None:
-        raise NotImplementedError
+        self.env = None
+        self.h_weight = 0
+
+    def manhattan_dist (self, state1, state2) -> int:
+        (state1_row, state1_col) = self.env.to_row_col(state1)
+        (state2_row, state2_col)  = self.env.to_row_col(state2)
+        return abs(state1_row - state2_row) + abs(state1_col - state2_col)
+
+
+    def hmsap(self, state):
+        distance_to_goals = []
+        distance_to_goals.append(self.manhattan_dist(state, self.env.d1))
+        distance_to_goals.append(self.manhattan_dist(state, self.env.d2))
+        for goal in self.env.goals:
+            distance_to_goals.append(self.manhattan_dist(state, goal))
+        return min(distance_to_goals)
 
     def search(self, env: DragonBallEnv, h_weight) -> Tuple[List[int], float, int]:
-        raise NotImplementedError
+        self.env = env
+        self.h_weight = h_weight
+        open = heapdict.heapdict()
+        close = heapdict.heapdict()
+        expanded = 0
+        state = self.env.get_initial_state()
+        rootNode = Node(None, state, None, -1, 1, self.env)
+        weight_avg = (1-h_weight) * rootNode.cumulative_cost + h_weight * self.hmsap(rootNode.state_tuple)
+        open[rootNode] = (weight_avg)
+        while len(open) > 0:
+            (current_node, weight_avg) = open.popitem()
+            close[current_node] = weight_avg
+            if (env.is_final_state(current_node.state_tuple)):
+                return get_actions(current_node), current_node.cumulative_cost, expanded
+            expanded += 1
+            print('expanded node:', current_node.state_tuple, '\n')
+            row, col = self.env.to_row_col(current_node.state_tuple)
+            if self.env.desc[row, col] != b"H":
+                for action, (state, cost, terminated) in env.succ(current_node.state_tuple).items():
+                    child_node = Node(current_node, state, action, current_node.cumulative_cost, cost, self.env)
+                    child_node.state_tuple = (child_node.state_tuple[0], current_node.state_tuple[1], current_node.state_tuple[2])
+                    if child_node.state_tuple[0] == env.d1[0]:
+                        child_node.state_tuple = (child_node.state_tuple[0], True, child_node.state_tuple[2])
+                    if child_node.state_tuple[0] == env.d2[0]:
+                        child_node.state_tuple = (child_node.state_tuple[0], child_node.state_tuple[1], True)
+                    new_weight = (1-h_weight) * child_node.cumulative_cost + h_weight * self.hmsap(child_node.state_tuple)
+                    if child_node.state_tuple not in [node.state_tuple for node in close.keys()] and child_node.state_tuple not in [node.state_tuple for node in open.keys()]:
+                        open[child_node] = new_weight
+                    if child_node.state_tuple in [node.state_tuple for node in open.keys()]:
+                        for key, value in open.items():
+                            if key.state_tuple == child_node.state_tuple:
+                                open_node = key
+                                break
+                        if new_weight < open[open_node]:
+                            open.pop(open_node)
+                            open[child_node] = new_weight
+
+                    if child_node.state_tuple in [node.state_tuple for node in close.keys()]:
+                        for key, value in close.items():
+                            if key.state_tuple == child_node.state_tuple:
+                                close_node = key
+                                break
+                        if new_weight < close[close_node]:
+                            close.pop(close_node)
+                            open[child_node] = new_weight
 
 
 
